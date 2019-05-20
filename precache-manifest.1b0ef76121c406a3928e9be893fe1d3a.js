679da20e8c97f5ae458d5204890d33138c8e7d55
FajarBay <fajarbayu90@gmail.com>
2019-05-20 21:21:53 +0700

    Updates

---
+++ precache-manifest.1b0ef76121c406a3928e9be893fe1d3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d469a80b503135adbaf42161483b0353",
+    "url": "/user/index.html"
+  },
+  {
+    "revision": "047a14b5f6d018014b8a",
+    "url": "/user/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/user/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "047a14b5f6d018014b8a",
+    "url": "/user/static/js/main.7e40d9cd.chunk.js"
+  },
+  {
+    "revision": "26090ba79986bd8cb115",
+    "url": "/user/static/js/runtime~main.872267ba.js"
+  }
+]);
