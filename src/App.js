ea5eb97ebc08674114e0592a8fd734af8f8931c4
FajarBay <fajarbayu90@gmail.com>
2019-05-20 21:14:09 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Septi from "./pages/Septi";
+import Rara from "./pages/Rara";
+import Daru from "./pages/Daru";
+import Fajar from "./pages/Fajar";
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      name : "Septi",
+    };
+  }
+  changeHandler(event) {
+    this.setState({
+      [event.target.name]: event.target.value 
+    });
+  }
+  clicked(menu){
+    this.setState({
+      name : menu.name,
+    });
+  }
+  info(name){    
+    if(name === 'Rara'){
+      return (
+        <Rara/>
+      );
+    } else if(name === 'Daru'){
+      return (
+        <Daru/>
+      );
+    } else if(name === 'Fajar'){
+      return (
+        <Fajar/>
+      );
+    } else {
+      //display default
+      return (
+        <Septi/>
+      );
+    }
+  }
+  render() { 
+    return ( 
+      <React.Fragment>
+        <div className="main-top">
+          <header>
+           <div className="container-fluid px-lg-5 ">
+               <nav className="mnu mx-auto">
+                <label htmlFor="drop" className="toggle">Anggota</label>
+                <ul className="menu">{
+                  this.props.pages.map((menu,index) => {
+                    let style = '';
+                    if(this.state.name === menu.name){
+                      style = `active`;
+                    }
+                    return <li                     
+                      className = { style }
+                      onClick = {this.clicked.bind(this,menu)} 
+                      key = {index}
+                    ><a className="mr-lg-4 mr-3">
+                      {menu.name}
+                    </a></li>;
+                  }
+                )}
+              </ul>
+              </nav>
+            </div>
+          </header>
+       </div>     
+
+        <div>
+            {this.info(this.state.name)}
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Rara.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Rara extends Component {
+    state = { }
+    render() {
+    return (
+        <React.Fragment>
+        <section className="banner-bottom" id="about">
+		<div className="container">
+            <div className="inner-sec">
+                <div className="row middle-grids">
+                    <div className="col-lg-4 advantage-grid-info1">
+                        <div className="advantage_left1 text-center">
+                            <img src="images/rara.png" className="img-fluid" alt=""></img>
+                        </div>
+                    </div> 
+					 <div className="col-lg-8 advantage-grid-info">
+                        <div className="advantage_left">
+                             <h1 className="heading text-capitalize mb-sm-5 mb-4">Hi. I'm <span>Azzahra.</span></h1>
+							<p className="mt-4">Integer porttitor <span>mollisar </span>lorem, at molestie arcu pulvinar ut. Proin ac fermentum est. Cras mi ipsum, consectetur.</p>
+							<a href="#services" className="banner-button btn mt-5 scroll">Learn about what i do</a>
+						</div>
+                    </div>
+                </div>
+            </div>
+		</div>
+    </section>
+    </React.Fragment>
+    );
+}
+}
+
+export default Rara;
