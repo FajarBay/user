33a441c06ad9ec0f6e3c5205ff95315e2d7114c9
FajarBay <fajarbayu90@gmail.com>
2019-05-20 22:53:25 +0700

    second

--- src/pages/Daru.js
+++ src/pages/Daru.js
@@ -17,8 +17,22 @@ class Daru extends Component {
 					 <div className="col-lg-8 advantage-grid-info">
                         <div className="advantage_left">
                              <h1 className="heading text-capitalize mb-sm-5 mb-4">Hi. I'm <span>Dewandaru.</span></h1>
-							<p className="mt-4">Integer porttitor <span>mollisar </span>lorem, at molestie arcu pulvinar ut. Proin ac fermentum est. Cras mi ipsum, consectetur.</p>
-							<a href="#services" className="banner-button btn mt-5 scroll">Learn about what i do</a>
+							<p className="mt-4">Integer porttitor <span>mollisar </span>
+                            lorem, at molestie arcu pulvinar ut. Proin ac fermentum est. Cras mi ipsum, consectetur.</p><br />
+							<table>
+                                <tr>
+                                    <td className="pb-1 pr-5"><p>Email</p></td>
+                                    <td><p>example@mail.com</p></td>
+                                </tr>
+                                <tr>
+                                    <td className="pb-1 pr-5"><p>Address</p></td>
+                                    <td><p>Yogyakarta</p></td>
+                                </tr>
+                                <tr>
+                                    <td className="pb-1 pr-5"><p>Telephone</p></td>
+                                    <td><p>0987654321</p></td>
+                                </tr>
+                            </table>
 						</div>
                     </div>
                 </div>
